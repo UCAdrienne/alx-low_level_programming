3f93afc2a3cb833c5df52c3332314d65c0d2d952
UCAdrienne <dbmbonu@gmail.com>
2022-09-08 15:26:45 -0700

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
-#include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ *  *  * main - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+		char myChar[55];
+
+			strcpy(myChar, "\"Programming is like building a multilingual puzzle");
+
+				puts(myChar);
+
+					return (0);
 }
