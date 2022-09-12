5385cb4da419ea3e417b6c28e7a032e05ae8fbd5
UCAdrienne <dbmbonu@gmail.com>
2022-09-12 16:55:50 -0700

    ok

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,16 +14,18 @@ int main(void)
 	int num;
 
 	for (num = 0; num <= 9; num++)
-	putchar((num % 10) + '0');
+	{
+		putchar((num % 10) + '0');
 
-	if (num == 9)
-	continue;
+		if (num == 9)
+		continue;
 
-	putchar(',');
+		putchar(',');
 
-	putchar(' ');
+		putchar(' ');
 
-	putchar('\n');
+		putchar('\n');
 
+	}
 	return (0);
 }
