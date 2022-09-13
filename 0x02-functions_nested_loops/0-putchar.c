e26ef91efa537020d73d4d95e4f350974d101d76
UCAdrienne <dbmbonu@gmail.com>
2022-09-13 10:50:38 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line.
+ * Description: using main function
+ * this program prints _putchar, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+
+{
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+								
+	return (0);
+}
