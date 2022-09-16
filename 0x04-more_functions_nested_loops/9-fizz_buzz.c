dcb51e758c3442b1237ea97ef435c24725e813da
UCAdrienne <dbmbonu@gmail.com>
2022-09-16 10:13:04 -0700

    nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -23,16 +23,16 @@ int main(void)
 		{
 			printf(" Buzz");
 		}
-	       	else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
 		}
-	       	else if (i == 1)
+		else if (i == 1)
 		{
 			printf("%d", i);
 
 		}
-	       	else
+		else
 		{
 			printf(" %d", i);
 		}
