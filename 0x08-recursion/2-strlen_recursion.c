e2db837ecdd44421e5dd3a6c531fa81a734ed139
UCAdrienne <dbmbonu@gmail.com>
2022-09-29 14:20:01 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen -returns the length of a string
+ * @s: string
+ * Return: the length of a string.
+*/
+
+int _strlen(char *s)
+
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
+
+}
